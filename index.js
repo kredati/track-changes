2663d4a93fa2bd13fa01476ebe1939ddf69e560f
Scott Richmond <kredati@users.noreply.github.com>
2022-07-22 13:56:57 -0400

    Add date/time reporting, add gitignore w/ binary file.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import {readLines} from "https://deno.land/std/io/bufio.ts";
-import {MuxAsyncIterator} from "https://deno.land/std/async/mux_async_iterator.ts";
-import {extname} from "https://deno.land/std/path/mod.ts";
+import { readLines } from "https://deno.land/std/io/bufio.ts";
+import { MuxAsyncIterator } from "https://deno.land/std/async/mux_async_iterator.ts";
+import { extname } from "https://deno.land/std/path/mod.ts";
 
 const watcher = Deno.watchFs(["./"]);
 
@@ -16,24 +16,27 @@ console.log("To quit, type 'close', 'quit', 'q', or 'exit'.");
 
 for await (const event of events) {
 
-	if (quit_phrases.includes(event)) Deno.exit();
-	if (typeof event === "string") continue;
-	
-	const path = event.paths[0];
+  if (quit_phrases.includes(event)) Deno.exit();
+  if (typeof event === "string") continue;
 
-	if (extname(path) === ".md") {
-		let proc = Deno.run({
-			cmd: ["git", "add", path],
-			stdout: "null"
-		});
+  const path = event.paths[0];
 
-		await proc.status();
+  if (extname(path) === ".md") {
+    let now = new Date(Date.now());
+    console.log(`Detected change in ${path}. Committing change at ${now.toLocaleString()}`);
 
-		proc = Deno.run({
-			cmd: ["git", "commit", "-m", `track-changes: Update ${path}`],
-			stdout: "null"
-		});
+    let proc = Deno.run({
+      cmd: ["git", "add", path],
+      stdout: "null"
+    });
 
-		await proc.status();
-	}
+    await proc.status();
+
+    proc = Deno.run({
+      cmd: ["git", "commit", "-m", `track-changes: Update ${path}`],
+      stdout: "null"
+    });
+
+    await proc.status();
+  }
 }
